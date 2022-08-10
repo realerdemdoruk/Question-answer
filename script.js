7379ce474e94ef6ecb2c80491d809c336bf7d74a
Erdem <erdemdoruk3@gmail.com>
2022-08-10 14:01:56 +0300

    Finish

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const questions = document.querySelectorAll(".question");
 
-console.log(questions);
-
 questions.forEach((question) => {
     question.addEventListener("click", function () {
         question.classList.toggle("active");
